de960385f04536e8c8d8c1eae9ed4f511e3e1740
Nelson Mungai <nelson.irungu@students.jkuat.ac.ke>
2024-02-20 22:04:10 +0300

    drawn the circle and moved the coordinate sytem to the centre of the window

--- MidPointCircle/circle.cpp
+++ MidPointCircle/circle.cpp
@@ -1,267 +1,70 @@
-// C++ program to implement onClick
-// functionality in OpenGL to draw
-// a circle using polar coordinates
-// and midpoint algorithm
-#include <GL/glut.h>
-#include <iostream>
-#include <math.h>
-#include <stdlib.h>
-#define xpix 500
-#include <cstring>
-using namespace std;
+// adjust the projection matrix using "gluOrtho2D to center the coordinate system around the center of the window
 
-float r, g, b, x, y;
-bool flag = true;
-int counter = 0;
+# include <GL/glut.h>
+#include<cmath>
 
-// Function to plot the coordinates
-void plot(int x1, int y1)
-{
-	// Initialize the points
+const int width=100;
+const int height=100;
+int radius=4;
+int x_center=3;
+int y_center=3;
+
+void midPointCircleDraw(){
+	int initPoint_x=0,init_point_y=radius;
+	int new_x=initPoint_x,new_y=init_point_y;
+	int plot_x=x_center+initPoint_x;
+	int plot_y=y_center+init_point_y;
+
+	int pk=1-radius;
+// Increase point size
+	glPointSize(3.0);
+
 	glBegin(GL_POINTS);
+	glVertex2i(plot_x,plot_y);
+	while(new_x<new_y){
+		new_x++;
+		if(pk<0){
+			pk+=2*new_x+1;
+		}else{
+			new_y--;
+			pk=pk+2*(new_x-new_y)+1;
+		}
+		glVertex2i(x_center+new_x,y_center+new_y);
+		glVertex2i(x_center-new_x,y_center+new_y);
+		glVertex2i(x_center+new_x,y_center-new_y);
+		glVertex2i(x_center-new_x,y_center-new_y);
+		glVertex2i(x_center+new_y,y_center+new_x);
+		glVertex2i(x_center-new_y,y_center+new_x);
+		glVertex2i(x_center+new_y,y_center-new_x);
+		glVertex2i(x_center-new_y,y_center-new_x);
 
-	// Draw the vertex
-	glVertex2i(x1 + x, y1 + y);
 
-	// End
+	}
 	glEnd();
 }
-
-// Function works on mouse click
-void mouse(int button, int state,
-		int mousex, int mousey)
-{
-	// Check if the current click
-	// is left-click or not
-	if (button == GLUT_LEFT_BUTTON
-		&& state == GLUT_DOWN) {
-		flag = true;
-		x = mousex;
-		y = 600 - mousey;
-	}
-
-	// Check if the current click
-	// is right-click or not
-	else if (button == GLUT_RIGHT_BUTTON
-			&& state == GLUT_DOWN) {
-		if (counter > 4) {
-			counter = 0;
-		}
-
-		counter++;
-
-		// Redisplay
-		glutPostRedisplay();
-	}
-}
-
-// Function that exits from program
-void keyboard(unsigned char key,
-			int x, int y)
-{
-	switch (key) {
-	case 27:
-		glutHideWindow();
-	}
-}
-
-// Function to draw the circles using
-// the midpoint circle algorithms
-void midPointCircleDraw(void)
-{
-	// Change the color of circles on
-	// the right click
-	if (counter == 1) {
-		glColor3f(1, 0, 0);
-	}
-	else if (counter == 2) {
-		glColor3f(0, 1, 0);
-	}
-	else if (counter == 3) {
-		glColor3f(0, 1, 1);
-	}
-	else if (counter == 4) {
-		glColor3f(0.5, 0, 1);
-	}
-	else if (counter == 5) {
-
-		glColor3f(0, 0.5, 1);
-	}
-
-	// Matrix mode
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	// Given the coordinates
-	gluOrtho2D(0.0, 400.0, 0.0, 600.0);
-	int r = 50;
-
-	if (flag) {
-
-		// Begin the pointer
-		glBegin(GL_POLYGON);
-		int x1 = 0;
-		int y1 = r;
-
-		// Initialising the value
-		// of the decision
-		float decision = 5 / 4 - r;
-
-		// Print the initial point on
-		// the axes after translation
-		plot(x1, y1);
-
-		while (y1 > x1) {
-
-			// Mid-point is inside or
-			// on the perimeter
-			if (decision < 0) {
-				x1++;
-				decision += 2 * x1 + 1;
-			}
-
-			// Mid-point is outside
-			// the perimeter
-			else {
-				y1--;
-				x1++;
-				decision += 2 * (x1 - y1) + 1;
-			}
-
-			// Plot the points in the
-			// different octants
-			plot(x1, y1);
-			plot(x1, -y1);
-			plot(-x1, y1);
-			plot(-x1, -y1);
-			plot(y1, x1);
-			plot(-y1, x1);
-			plot(y1, -x1);
-			plot(-y1, -x1);
-		}
-	}
-
+void display(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor4f(1.0,0.0,0.0,1.0);
+	midPointCircleDraw();
 	glFlush();
 }
-
-// Function to draw the circle using
-// the polar coordinates
-void polarCoordinateCircleDraw(void)
-{
-	float angle_theta;
-
-	// Change the color of the circles
-	// on right click
-	if (counter == 1) {
-		glColor3f(1, 0, 0);
-	}
-	else if (counter == 2) {
-		glColor3f(0, 1, 0);
-	}
-	else if (counter == 3) {
-		glColor3f(0, 1, 1);
-	}
-	else if (counter == 4) {
-		glColor3f(0.5, 0, 1);
-	}
-	else if (counter == 5) {
-
-		glColor3f(0, 0.5, 1);
-	}
-
-	// Matrix mode
+// CALLED once to perform initialization tasks
+void init(){
+	glClearColor(1.0,1.0,1.0,1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
-	// Given the coordinates
-	gluOrtho2D(0.0, 800.0, 0.0, 600.0);
-	if (flag) {
-
-		// Begin the pointer
-		glBegin(GL_POLYGON);
-
-		// Iterate through all the
-		// 360 degrees
-		for (int i = 0; i < 360; i++) {
-
-			angle_theta = i * 3.142 / 180;
-			glVertex2f(x + 50 * cos(angle_theta),
-					y + 50 * sin(angle_theta));
-		}
-
-		// Set the vertex
-		glEnd();
-	}
-
-	// Flushes the frame buffer to
-	// the screen
-	glFlush();
+	// set up 2D Orthographic viewing region centred at (width/2,height/2)
+	gluOrtho2D(-100/2,100/2,-100/2,100/2);
 }
-
-// Driver Code
-int main(int argc, char** argv)
-{
-	// Initialize the drivers
-	glutInit(&argc, argv);
-
-	// Initialize the display mode
-	glutInitDisplayMode(
-		GLUT_SINGLE | GLUT_RGB);
-
-	// Update the window size
-	glutInitWindowSize(800, 600);
-
-	// Update the window position
-	glutInitWindowPosition(100, 100);
-
-	int mainWindow = glutCreateWindow(
-		"Circle Generation using "
-		"Midpoint Algorithm on "
-		"left and using Polar "
-		"Coordinates on right");
-
-	// Set the background color of
-	// the right sub-window
-	glClearColor(0, 0, 0, 0);
-
-	// Clears the frame buffer
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	// Links display event with the
-	// display the event handler
-	// (polarCoordinateCircleDraw)
-	glutDisplayFunc(
-		polarCoordinateCircleDraw);
-
-	// Mouse event handler
-	glutMouseFunc(mouse);
-
-	// Create the sub-window to implement
-	// midpoint algorithm on left
-	int subWindow1
-		= glutCreateSubWindow(
-			mainWindow, 0, 0, 400, 600);
-
-	glutInitWindowPosition(100, 100);
-
-	// Sets the background color of
-	// the left sub-window
-	glClearColor(1, 0.5, 0.5, 1);
-
-	// Clears the frame buffer
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	// Links display event with the
-	// display the event handler
-	// (midPointCircleDraw)
-	glutDisplayFunc(midPointCircleDraw);
-
-	// Mouse event handler
-	glutMouseFunc(mouse);
-
-	// Keyboard event handler
-	glutKeyboardFunc(keyboard);
-
-	// Loops the current event
+// main driver function
+int main(int argc,char**argv){
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(100,100);
+	glutInitWindowPosition(100,100);
+	glutCreateWindow("Mid Point Circle");
+	init();
+	glutDisplayFunc(display);
 	glutMainLoop();
+	return 0;
 }
